fdd2170f589277dbac2280031084ee29514b9191
guridaek <iue16@naver.com>
2022-12-14 18:50:15 +0900

    feat: 로또 구매 기능 구현

---
+++ src/LottoGame.js
@@ -0,0 +1,19 @@
+const { Random } = require('@woowacourse/mission-utils');
+const Lotto = require('./Lotto');
+
+class LottoGame {
+  #lottos;
+  #winningLotto;
+
+  constructor() {
+    this.#lottos = [];
+  }
+
+  purchaseLottos(budget) {
+    for (let i = 0; i < budget / 1000; i += 1) {
+      this.#lottos.push(new Lotto(Random.pickUniqueNumbersInRange(1, 45, 6)));
+    }
+  }
+}
+
+module.exports = LottoGame;
